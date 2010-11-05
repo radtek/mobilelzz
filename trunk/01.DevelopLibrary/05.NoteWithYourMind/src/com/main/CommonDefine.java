9f880acc05df38e2c831bcce6f9a7382e7d643f8
lidan8908589 <lidan8908589@8983de3c-2b35-11df-be6c-f52728ce0ce6>
2010-11-05 16:08:02 +0000

    infect zhangdan

---
+++ trunk/01.DevelopLibrary/05.NoteWithYourMind/src/com/main/CommonDefine.java
@@ -0,0 +1,6 @@
+package com.main;
+
+class CommonDefine{
+	public static int g_int_Invalid_ID = -1;
+	
+}
---
+++ trunk/01.DevelopLibrary/05.NoteWithYourMind/src/com/main/ViewListInFolder.java
@@ -0,0 +1,52 @@
+package com.main;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ViewListInFolder extends Activity
+{
+	public static String ExtraData_FolderID = "com.main.ExtraData_FolderID";
+	private int m_Cur_FolderID = CommonDefine.g_int_Invalid_ID;
+	private NoteListUICtrl m_NoteListUICtrl;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.folderview);	
+		Intent iExtraData = this.getIntent();
+		m_Cur_FolderID = iExtraData.getIntExtra(ExtraData_FolderID, CommonDefine.g_int_Invalid_ID );
+		
+		TextView tvFolderName = (TextView)findViewById(R.id.memolisttitle_infolder);
+		CNoteDBCtrl clCNoteDBCtrl = new CNoteDBCtrl(this);
+		Cursor folderRec = clCNoteDBCtrl.getMemoRec(m_Cur_FolderID);
+		if(folderRec.getCount()>0){
+			folderRec.moveToFirst();
+			int index = folderRec.getColumnIndex(CNoteDBCtrl.KEY_detail);
+			String folderName = folderRec.getString(index);
+			tvFolderName.setText(folderName);
+		}
+		ListView memoList = (ListView) findViewById(R.id.listviewmemo);
+		LinearLayout toolbarLayout = (LinearLayout) findViewById(R.id.memolistmenu);
+		m_NoteListUICtrl = new NoteListUICtrl(this, memoList, m_Cur_FolderID, toolbarLayout);
+		m_NoteListUICtrl.initializeSource();
+	}
+	public void onStop()
+	{
+		super.onStop();
+		//m_NoteListUICtrl.releaseSource();
+		//m_NoteListUICtrl = null;
+	}
+	public void onDestroy()
+	{
+		super.onDestroy();
+		m_NoteListUICtrl.releaseSource();
+		m_NoteListUICtrl = null;
+	}
+}
